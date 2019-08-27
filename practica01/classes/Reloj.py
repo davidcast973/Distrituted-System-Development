a625a800d6f0238a6f3f9def557813df7e3e84b7
Angel Rmx <aramireza1506@alumno.ipn.mx>
2019-08-27 18:06:10 -0500

    Se corrigen errores de REloj y se trata de conectar a edicion

--- practica01/classes/Reloj.py
+++ practica01/classes/Reloj.py
@@ -1,6 +1,7 @@
 import threading
 import time
 import random
+import datetime
 #creamos la clase Reloj.
 #Reloj extiende (hereda de la clase threading.Thread)
 class Reloj(threading.Thread):
@@ -9,6 +10,7 @@ class Reloj(threading.Thread):
 	hora = -1
 	mins = -1
 	segs = -1
+	ritmo = 2
 
 	#El constructor de la clase recibe el nombre que tendrá el reloj.
 	def __init__(self,nombre, hora=None, mins=None, segs=None):
@@ -16,19 +18,20 @@ class Reloj(threading.Thread):
 		#Se asigna el nombre al hilo
 		self.nombre= nombre
 		if(hora!=None and mins!=None and segs!=None):
-    		self.hora=hora
-       		self.mins=mins
-       		self.segs=segs
-        else:
-            if(self.nombre="0"):
-                t=datetime.datetime.now()
-                self.hora = t.hour
-                self.mins = t.minute
-                self.segs = t.second
-            else:
-                self.hora=random.randint(13)
-                self.mins=random.randint(60)
-                self.segs=random.randint(60)
+			self.hora=hora
+			self.mins=mins
+			self.segs=segs
+		else:
+			if(self.nombre=="#1"):
+				t = datetime.datetime.now()
+				self.hora = t.hour -1
+				self.mins = t.minute -1 
+				self.segs = t.second -1
+				print("Hora de este:", self )
+			else:
+				self.hora=random.randint(0,24)
+				self.mins=random.randint(0,60)
+				self.segs=random.randint(0,60)
 
 	#Se sobreescribe el método run para poder correlo como hilo
 	def run(self):
@@ -36,18 +39,21 @@ class Reloj(threading.Thread):
 		No es más que un grupo de while's anidados.
 		"""
 		while True:
-			while self.hora< 12:
-				self.hora +=1
+			while self.hora< 24:
+				self.hora += 1
 				while self.mins< 60:
 					self.mins+= 1
 					while self.segs< 60:
 						self.segs+= 1
 						#Se imprime el valor del reloj para verificar su funcionalidad
 						print("reloj:",self.nombre,"->",self)
-						time.sleep(1)
+						time.sleep(self.ritmo)
 					self.segs= -1
 				self.mins= -1
 			self.hora= -1
+		
+	def cambiaRitmo(self, nuevoRitmo):
+		self.ritmo=nuevoRitmo
 
 	#Se sobreescribe su método __repr__
 	def __repr__(self):
--- practica01/static/js/infoHilos.js
+++ practica01/static/js/infoHilos.js
@@ -88,6 +88,257 @@ $(function(){
         console.log(data);
         });
     }, 1000);
+
+
+    $("#edit2").click(function(){
+        clearInterval(a);
+        swal({
+            text: 'Dame la nueva hora en formato HH:MM:SS',
+            content: "input",
+            button: {
+              text: "Editar!",
+              closeModal: true,
+            },
+          })
+          .then(name => {
+            if (!name) throw null;
+            tiempo=name.split(":");
+            $.ajax({
+                type: "GET",
+                url: `/relojes/edit/${0}/${tiempo[0]}/${tiempo[1]}/${tiempo[2]}`,
+                //data: JSON.stringify({'auditoria':idAuditoria , 'preguntas':misCambios}),
+                contentType:'application/json;charset=UTF-8',    
+            })
+            .done(function(data) {
+                a = setInterval(function(){
+                    $.ajax({
+                        type: "GET",
+                        url: '/relojes/getTime/0/',
+                        //data: JSON.stringify({'auditoria':idAuditoria , 'preguntas':misCambios}),
+                        contentType:'application/json;charset=UTF-8',    
+                    })
+                    .done(function(data) {
+                        console.log( "exito R1" );
+                        console.log(data);
+                        
+                        var horaFormat = "";
+                        var horaR1 = data.description.tiempo;
+                        horaFormat = horaR1.hora+":"+horaR1.mins+":"+horaR1.segs;
+                        document.getElementById('reloj1').innerHTML = horaFormat;
+            
+                    })
+                    .fail(function(data) {
+                    console.log( "error" );
+                    console.log(data);
+                    });
+                }, 1000);
+            })
+            .fail(function(data) {
+            console.log( "error" );
+            console.log(data);
+            });
+          })
+    });
+    $("#edit2").click(function(){
+        clearInterval(a);
+        swal({
+            text: 'Dame la nueva hora en formato HH:MM:SS',
+            content: "input",
+            button: {
+              text: "Editar!",
+              closeModal: true,
+            },
+          })
+          .then(name => {
+            if (!name) throw null;
+            tiempo=name.split(":");
+            $.ajax({
+                type: "GET",
+                url: `/relojes/edit/${1}/${tiempo[0]}/${tiempo[1]}/${tiempo[2]}`,
+                //data: JSON.stringify({'auditoria':idAuditoria , 'preguntas':misCambios}),
+                contentType:'application/json;charset=UTF-8',    
+            })
+            .done(function(data) {
+                a = setInterval(function(){
+                    $.ajax({
+                        type: "GET",
+                        url: '/relojes/getTime/0/',
+                        //data: JSON.stringify({'auditoria':idAuditoria , 'preguntas':misCambios}),
+                        contentType:'application/json;charset=UTF-8',    
+                    })
+                    .done(function(data) {
+                        console.log( "exito R1" );
+                        console.log(data);
+                        
+                        var horaFormat = "";
+                        var horaR1 = data.description.tiempo;
+                        horaFormat = horaR1.hora+":"+horaR1.mins+":"+horaR1.segs;
+                        document.getElementById('reloj1').innerHTML = horaFormat;
+            
+                    })
+                    .fail(function(data) {
+                    console.log( "error" );
+                    console.log(data);
+                    });
+                }, 1000);
+            })
+            .fail(function(data) {
+            console.log( "error" );
+            console.log(data);
+            });
+          })
+    });
+    
+    $("#edit2").click(function(){
+        clearInterval(b);
+        swal({
+            text: 'Dame la nueva hora en formato HH:MM:SS',
+            content: "input",
+            button: {
+              text: "Editar!",
+              closeModal: true,
+            },
+          })
+          .then(name => {
+            if (!name) throw null;
+            tiempo=name.split(":");
+            $.ajax({
+                type: "GET",
+                url: `/relojes/edit/${0}/${tiempo[0]}/${tiempo[1]}/${tiempo[2]}`,
+                //data: JSON.stringify({'auditoria':idAuditoria , 'preguntas':misCambios}),
+                contentType:'application/json;charset=UTF-8',    
+            })
+            .done(function(data) {
+                b = setInterval(function(){
+                    $.ajax({
+                        type: "GET",
+                        url: '/relojes/getTime/0/',
+                        //data: JSON.stringify({'auditoria':idAuditoria , 'preguntas':misCambios}),
+                        contentType:'application/json;charset=UTF-8',    
+                    })
+                    .done(function(data) {
+                        console.log( "exito R1" );
+                        console.log(data);
+                        
+                        var horaFormat = "";
+                        var horaR1 = data.description.tiempo;
+                        horaFormat = horaR1.hora+":"+horaR1.mins+":"+horaR1.segs;
+                        document.getElementById('reloj1').innerHTML = horaFormat;
+            
+                    })
+                    .fail(function(data) {
+                    console.log( "error" );
+                    console.log(data);
+                    });
+                }, 1000);
+            })
+            .fail(function(data) {
+            console.log( "error" );
+            console.log(data);
+            });
+          })
+    });
+    $("#edit3").click(function(){
+        clearInterval(c);
+        swal({
+            text: 'Dame la nueva hora en formato HH:MM:SS',
+            content: "input",
+            button: {
+              text: "Editar!",
+              closeModal: true,
+            },
+          })
+          .then(name => {
+            if (!name) throw null;
+            tiempo=name.split(":");
+            $.ajax({
+                type: "GET",
+                url: `/relojes/edit/${2}/${tiempo[0]}/${tiempo[1]}/${tiempo[2]}`,
+                //data: JSON.stringify({'auditoria':idAuditoria , 'preguntas':misCambios}),
+                contentType:'application/json;charset=UTF-8',    
+            })
+            .done(function(data) {
+                c = setInterval(function(){
+                    $.ajax({
+                        type: "GET",
+                        url: '/relojes/getTime/0/',
+                        //data: JSON.stringify({'auditoria':idAuditoria , 'preguntas':misCambios}),
+                        contentType:'application/json;charset=UTF-8',    
+                    })
+                    .done(function(data) {
+                        console.log( "exito R1" );
+                        console.log(data);
+                        
+                        var horaFormat = "";
+                        var horaR1 = data.description.tiempo;
+                        horaFormat = horaR1.hora+":"+horaR1.mins+":"+horaR1.segs;
+                        document.getElementById('reloj1').innerHTML = horaFormat;
+            
+                    })
+                    .fail(function(data) {
+                    console.log( "error" );
+                    console.log(data);
+                    });
+                }, 1000);
+            })
+            .fail(function(data) {
+            console.log( "error" );
+            console.log(data);
+            });
+          })
+    });
+    $("#edit4").click(function(){
+        clearInterval(c);
+        swal({
+            text: 'Dame la nueva hora en formato HH:MM:SS',
+            content: "input",
+            button: {
+              text: "Editar!",
+              closeModal: true,
+            },
+          })
+          .then(name => {
+            if (!name) throw null;
+            tiempo=name.split(":");
+            $.ajax({
+                type: "GET",
+                url: `/relojes/edit/${3}/${tiempo[0]}/${tiempo[1]}/${tiempo[2]}`,
+                //data: JSON.stringify({'auditoria':idAuditoria , 'preguntas':misCambios}),
+                contentType:'application/json;charset=UTF-8',    
+            })
+            .done(function(data) {
+                c = setInterval(function(){
+                    $.ajax({
+                        type: "GET",
+                        url: '/relojes/getTime/0/',
+                        //data: JSON.stringify({'auditoria':idAuditoria , 'preguntas':misCambios}),
+                        contentType:'application/json;charset=UTF-8',    
+                    })
+                    .done(function(data) {
+                        console.log( "exito R1" );
+                        console.log(data);
+                        
+                        var horaFormat = "";
+                        var horaR1 = data.description.tiempo;
+                        horaFormat = horaR1.hora+":"+horaR1.mins+":"+horaR1.segs;
+                        document.getElementById('reloj1').innerHTML = horaFormat;
+            
+                    })
+                    .fail(function(data) {
+                    console.log( "error" );
+                    console.log(data);
+                    });
+                }, 1000);
+            })
+            .fail(function(data) {
+            console.log( "error" );
+            console.log(data);
+            });
+          })
+    });
+
+
+
     /*b = setInterval(function(){
 
     })
